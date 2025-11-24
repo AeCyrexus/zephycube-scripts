2e7b76aefcf182cd1a3869a8a5fea5a6df0e93b8
AeCyrexus <kidstech_02@yahoo.com>
2025-11-24 20:53:21 +0800

    Update apt-cacher-ng.sh

--- apt-cacher-ng.sh
+++ apt-cacher-ng.sh
@@ -1,7 +1,7 @@
 sudo tee /usr/local/bin/apt-proxy-detect.sh > /dev/null << 'EOF' && sudo chmod +x /usr/local/bin/apt-proxy-detect.sh && echo 'Acquire::http::Proxy-Auto-Detect "/usr/local/bin/apt-proxy-detect.sh";' | sudo tee /etc/apt/apt.conf.d/00aptproxy > /dev/null
 #!/bin/bash
-if nc -w1 -z "10.0.0.6" 3142; then
-  echo -n "http://10.0.0.6:3142"
+if nc -w1 -z "10.10.10.5" 3142; then
+  echo -n "http://10.10.10.5:3142"
 else
   echo -n "DIRECT"
 fi
